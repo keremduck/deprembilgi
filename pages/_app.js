92b9a00bd62656ecdfdf2040391da2ffd5ee3ee3
Yusuf <66310246+yuswf@users.noreply.github.com>
2023-02-10 13:33:41 +0300

    Delete pages directory

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '@/styles/globals.css'
-import '../styles/Card.css'
-import '../styles/Loader.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
