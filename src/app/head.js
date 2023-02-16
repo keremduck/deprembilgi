1792454d2b6ab1218768e0bec2a0ffa3db066ed5
None <usern0ne@yandex.com>
2023-02-16 22:43:48 +0300

    Update head.js

--- src/app/head.js
+++ src/app/head.js
@@ -5,6 +5,8 @@ export default function Head() {
             <meta content="width=device-width, initial-scale=1" name="viewport"/>
             <meta name="description" content="Türkiye için deprem bilgileri."/>
             <link rel="icon" href="/icon.svg"/>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3365692366727044"
+            crossorigin="anonymous"></script>
         </>
     );
 }
